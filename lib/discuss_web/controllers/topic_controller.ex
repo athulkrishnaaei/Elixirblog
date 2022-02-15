e2346648b8fb7357af1be818eafa15bafeca65f4
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2022-02-15 20:30:26 +0530

    Update topic_controller.ex

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,7 +2,7 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Dicuss.Topic
 
-  def index(conn, _params) do
+  def new(conn, _params) do
 
     changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html")
