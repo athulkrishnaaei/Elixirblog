75a6f898ec425ee251f8debe35f47649578c6d31
athulkrishnakdgr@gmail.com <athulkrishnakdgr@gmail.com>
2022-02-15 13:30:14 +0530

    hello

---
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,12 @@
+defmodule Discuss.TopicController do
+  use DiscussWeb, :controller
+
+  def new(conn, params) do
+    IO.puts "++++"
+    IO.inspect conn
+    IO.puts "++++"
+    IO.inspect params
+    IO.puts "+++"
+
+  end
+end
