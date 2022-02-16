9731bc015664c2d8662a1c326e320699721f1122
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2022-02-16 10:54:33 +0530

    css changed

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,6 +1,7 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Topic
+  alias Discuss.Repo
 
   def new(conn, _params) do
 
@@ -9,6 +10,12 @@ defmodule DiscussWeb.TopicController do
     render(conn, "new.html", changeset: changeset)
   end
   def create(conn, %{"topic" => topic}) do
+    changeset =Topic.changeset(%Topic{}, topic)
 
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
