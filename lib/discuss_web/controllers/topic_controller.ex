de0d39768b2cfef69455563d4b7e5ca60d6ad43b
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2022-02-16 11:57:15 +0530

    added link to add new topic page to homepage

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -3,17 +3,25 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+
   def new(conn, _params) do
-
     changeset = Topic.changeset(%Topic{}, %{})
-
     render(conn, "new.html", changeset: changeset)
   end
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def create(conn, %{"topic" => topic}) do
     changeset =Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
