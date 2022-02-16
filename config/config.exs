e8fb7f6b9108d0c87e2efddd63d3fbc56d65cc5b
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2022-02-16 18:03:37 +0530

    githu authentica

--- config/config.exs
+++ config/config.exs
@@ -50,3 +50,13 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+
+config :ueberauth, Ueberauth,
+   providers: [
+     github: {Ueberauth.Strategy.Github, []}
+   ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+   client_id: "370275ee8536346c9cbb",
+   client_secret: "6c2142b828c286eebba6ae66d999096862c8c703"
---
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -0,0 +1,13 @@
+defmodule DicussWeb.AuthController do
+  use DiscussWeb, :controller
+  plug Ueberauth
+
+
+  def callback(conn, params) do
+    IO.puts("++++++")
+    IO.inspect(conn.assigns)
+    IO.puts("++++++")
+    IO.inspect(params)
+    IO.puts("++++++")
+  end
+end
