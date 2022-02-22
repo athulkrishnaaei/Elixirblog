bcc2f8de3ce54100bc27e23268dfa1ed67d80cb7
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2022-02-22 13:32:48 +0530

    intialised

--- lib/discuss_web/models/topic.ex
+++ lib/discuss_web/models/topic.ex
@@ -1,12 +1,12 @@
 defmodule Discuss.Topic do
  import Ecto.Query
  use DiscussWeb, :model
-
+ alias DiscussWeb.Comment
 
   schema "topics" do
     field :title, :string
     belongs_to :user, DiscussWeb.User
-    has_many :comments, DicussWeb.Comment
+    has_many :comments, Comment
   end
 
   def changeset(struct, params \\ %{}) do
